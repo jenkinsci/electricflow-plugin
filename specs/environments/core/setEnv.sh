c1693cbdae19f7ead679e2990851ee4f522199a0
Anton Horodchuk <antoman1994@gmail.com>
2020-06-16 16:33:08 +0300

    Removing the core environment

--- specs/environments/core/setEnv.sh
+++
@@ -1,7 +0,0 @@
-export JENKINS_URL=http://35.202.33.179.beesdns.com
-export JENKINS_USERNAME=flowplugin
-export JENKINS_PASSWORD=ecloud1!
-export AUTOMATION_TESTS_CONTEXT_RUN=Jenkins:Sanity,E2E
-export PLUGIN_VERSION=1.17.0
-export JENKINS_TYPE=CORE
-export CORE_TEAM=flowpluginqeteam
